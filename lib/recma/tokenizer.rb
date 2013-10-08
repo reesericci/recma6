966f30cc417d293d7595daff9ba0466f283cd242
Rene Saarsoo <nene@triin.net>
2013-10-08 16:09:56 +0300

    Update list of reserved words to match ECMAScript 5.
    ES5 removed a bunch of reserved words that ES3 had reserved.  These
    are also supported as identifiers by real-world implementations. So
    removing these.
    
    ES5 added 'let' and 'yield' to the reserved list, which have been added
    in here too.

--- lib/recma/tokenizer.rb
+++ lib/recma/tokenizer.rb
@@ -1,23 +1,26 @@
-require 'recma/lexeme'
-require 'recma/char_range'
+require 'rkelly/lexeme'
 require 'strscan'
 
-module RECMA
+module RKelly
   class Tokenizer
-    KEYWORDS = Hash[%w{
+    KEYWORDS = %w{
       break case catch continue default delete do else finally for function
-      if in instanceof new return switch this throw try typeof var void while
-      with
+      if in instanceof new return switch this throw try typeof var void while 
+      with 
 
       const true false null debugger
-    }.map {|kw| [kw, kw.upcase.to_sym] }]
+    }
 
-    RESERVED = Hash[%w{
-      abstract boolean byte char class double enum export extends
-      final float goto implements import int interface long native package
-      private protected public short static super synchronized throws
-      transient volatile
-    }.map {|kw| [kw, true] }]
+    # First 6 are always reserved in ECMAScript 5.1
+    # Others are only reserved in strict mode.
+    # http://www.ecma-international.org/ecma-262/5.1/#sec-7.6.1.2
+    # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Reserved_Words
+    RESERVED = %w{
+      class enum export extends import super
+
+      implements interface package private protected public static
+      let yield
+    }
 
     LITERALS = {
       # Punctuators
@@ -49,56 +52,43 @@ module RECMA
 
     # Some keywords can be followed by regular expressions (eg, return and throw).
     # Others can be followed by division.
-    KEYWORDS_THAT_IMPLY_DIVISION = {
-      'this' => true,
-      'true' => true,
-      'false' => true,
-      'null' => true,
+    KEYWORDS_THAT_IMPLY_DIVISION = %w{
+      this true false null
     }
 
-    KEYWORDS_THAT_IMPLY_REGEX = KEYWORDS.reject {|k,v| KEYWORDS_THAT_IMPLY_DIVISION[k] }
+    KEYWORDS_THAT_IMPLY_REGEX = KEYWORDS - KEYWORDS_THAT_IMPLY_DIVISION
 
-    SINGLE_CHARS_THAT_IMPLY_DIVISION = {
-      ')' => true,
-      ']' => true,
-      '}' => true,
-    }
-
-    # Determine the method to use to measure String length in bytes,
-    # because StringScanner#pos can obly be set in bytes.
-    #
-    # - In Ruby 1.8 String#length returns always the string length
-    #   in bytes.
-    #
-    # - In Ruby 1.9+ String#length returns string length in
-    #   characters and we need to use String#bytesize instead.
-    #
-    BYTESIZE_METHOD = "".respond_to?(:bytesize) ? :bytesize : :length
+    SINGLE_CHARS_THAT_IMPLY_DIVISION = [')', ']', '}']
 
     def initialize(&block)
-      @lexemes = Hash.new {|hash, key| hash[key] = [] }
+      @lexemes = []
 
-      token(:COMMENT, /\/(?:\*(?:.)*?\*\/|\/[^\n]*)/m, ['/'])
-      token(:STRING, /"(?:[^"\\]*(?:\\.[^"\\]*)*)"|'(?:[^'\\]*(?:\\.[^'\\]*)*)'/m, ["'", '"'])
-      token(:S, /\s*/m, [" ", "\t", "\r", "\n", "\f"])
+      token(:COMMENT, /\A\/(?:\*(?:.)*?\*\/|\/[^\n]*)/m)
+      token(:STRING, /\A"(?:[^"\\]*(?:\\.[^"\\]*)*)"|\A'(?:[^'\\]*(?:\\.[^'\\]*)*)'/m)
 
       # A regexp to match floating point literals (but not integer literals).
-      digits = ('0'..'9').to_a
-      token(:NUMBER, /\d+\.\d*(?:[eE][-+]?\d+)?|\d+(?:\.\d*)?[eE][-+]?\d+|\.\d+(?:[eE][-+]?\d+)?/m, digits+['.']) do |type, value|
+      token(:NUMBER, /\A\d+\.\d*(?:[eE][-+]?\d+)?|\A\d+(?:\.\d*)?[eE][-+]?\d+|\A\.\d+(?:[eE][-+]?\d+)?/m) do |type, value|
         value.gsub!(/\.(\D)/, '.0\1') if value =~ /\.\w/
         value.gsub!(/\.$/, '.0') if value =~ /\.$/
         value.gsub!(/^\./, '0.') if value =~ /^\./
         [type, eval(value)]
       end
-      token(:NUMBER, /0[xX][\da-fA-F]+|0[0-7]*|\d+/, digits) do |type, value|
+      token(:NUMBER, /\A0[xX][\da-fA-F]+|\A0[0-7]*|\A\d+/) do |type, value|
         [type, eval(value)]
       end
 
-      word_chars = ('a'..'z').to_a + ('A'..'Z').to_a + ['_', '$']
-      token(:RAW_IDENT, /([_\$A-Za-z][_\$0-9A-Za-z]*)/, word_chars) do |type,value|
-        if KEYWORDS[value]
-          [KEYWORDS[value], value]
-        elsif RESERVED[value]
+      token(:LITERALS,
+        Regexp.new(LITERALS.keys.sort_by { |x|
+          x.length
+        }.reverse.map { |x| "\\A#{x.gsub(/([|+*^])/, '\\\\\1')}" }.join('|')
+      )) do |type, value|
+        [LITERALS[value], value]
+      end
+
+      token(:RAW_IDENT, /\A([_\$A-Za-z][_\$0-9A-Za-z]*)/) do |type,value|
+        if KEYWORDS.include?(value)
+          [value.upcase.to_sym, value]
+        elsif RESERVED.include?(value)
           [:RESERVED, value]
         else
           [:IDENT, value]
@@ -114,31 +104,10 @@ module RECMA
       # (eg, // and //g). Here we could depend on match length and priority to
       # determine that these are actually comments, but it turns out to be
       # easier to not match them in the first place.
-      token(:REGEXP, %r{
-             /                  (?# beginning )
+      token(:REGEXP, /\A\/(?:[^\/\r\n\\*]|\\[^\r\n])[^\/\r\n\\]*(?:\\[^\r\n][^\/\r\n\\]*)*\/[gim]*/)
+      token(:S, /\A[\s\r\n]*/m)
 
-             (?:
-               [^\r\n\[/\\]+      (?# any char except \r \n [ / \ )
-               |
-               \\ [^\r\n]         (?# escape sequence )
-               |
-               \[ (?:[^\]\\]|\\.)* \]   (?# [...] can contain any char including / )
-                                        (?# only \ and ] have to be escaped here )
-             )+
-
-             /[gim]*            (?# ending + modifiers )
-      }x, ['/'])
-
-      literal_chars = LITERALS.keys.map {|k| k.slice(0,1) }.uniq
-      literal_regex = Regexp.new(LITERALS.keys.sort_by { |x|
-          x.length
-        }.reverse.map { |x| "#{x.gsub(/([|+*^])/, '\\\\\1')}" }.join('|'))
-      token(:LITERALS, literal_regex, literal_chars) do |type, value|
-        [LITERALS[value], value]
-      end
-
-      symbols = ('!'..'/').to_a + (':'..'@').to_a + ('['..'^').to_a + ['`'] + ('{'..'~').to_a
-      token(:SINGLE_CHAR, /./, symbols) do |type, value|
+      token(:SINGLE_CHAR, /\A./) do |type, value|
         [value, value]
       end
     end
@@ -150,52 +119,46 @@ module RECMA
     def raw_tokens(string)
       scanner = StringScanner.new(string)
       tokens = []
-      range = CharRange::EMPTY
+      line_number = 1
       accepting_regexp = true
       while !scanner.eos?
-        token = match_lexeme(scanner, accepting_regexp)
+        longest_token = nil
 
-        if token.name != :S
-          accepting_regexp = followable_by_regex(token)
+        @lexemes.each { |lexeme|
+          next if lexeme.name == :REGEXP && !accepting_regexp
+
+          match = lexeme.match(scanner)
+          next if match.nil?
+          longest_token = match if longest_token.nil?
+          next if longest_token.value.length >= match.value.length
+          longest_token = match
+        }
+
+        if longest_token.name != :S
+          accepting_regexp = followable_by_regex(longest_token)
         end
 
-        scanner.pos += token.value.send(BYTESIZE_METHOD)
-        token.range = range = range.next(token.value)
-        tokens << token
+        longest_token.line = line_number
+        line_number += longest_token.value.scan(/\n/).length
+        scanner.pos += longest_token.value.length
+        tokens << longest_token
       end
       tokens
     end
-
+  
     private
-
-    # Returns the token of the first matching lexeme
-    def match_lexeme(scanner, accepting_regexp)
-      @lexemes[scanner.peek(1)].each do |lexeme|
-        next if lexeme.name == :REGEXP && !accepting_regexp
-
-        token = lexeme.match(scanner)
-        return token if token
-      end
-    end
-
-    # Registers a lexeme and maps it to all the characters it can
-    # begin with.  So later when scanning the source we only need to
-    # match those lexemes that can begin with the character we're at.
-    def token(name, pattern, chars, &block)
-      lexeme = Lexeme.new(name, pattern, &block)
-      chars.each do |c|
-        @lexemes[c] << lexeme
-      end
+    def token(name, pattern = nil, &block)
+      @lexemes << Lexeme.new(name, pattern, &block)
     end
 
     def followable_by_regex(current_token)
       case current_token.name
       when :RAW_IDENT
-        KEYWORDS_THAT_IMPLY_REGEX[current_token.value]
+        KEYWORDS_THAT_IMPLY_REGEX.include?(current_token.value)
       when :NUMBER
         false
       when :SINGLE_CHAR
-        !SINGLE_CHARS_THAT_IMPLY_DIVISION[current_token.value]
+        !SINGLE_CHARS_THAT_IMPLY_DIVISION.include?(current_token.value)
       else
         true
       end
