tenderlove/recma

Replace array-based ranges with CharRange and CharPos.
Create classes for holding character positions data.

- The CharPos contains both line/char and absolute character index.
- The CharRange combines two CharPos objects.
- Both have #next method to abstract away the creation
  of the ranges inside Tokenizer.

lib/rkelly/char_pos.rb
@@ -1,0 +1,33 @@
+
+module RKelly
+  # Represents a character position in source code.
+  #
+  # It's a value object - it can't be modified.
+  class CharPos
+    attr_reader :line, :char, :index
+
+    def initialize(line, char, index)
+      @line = line
+      @char = char
+      @index = index
+    end
+
+    # Creates a new character position that's a given string away from
+    # this one.
+    def next(string)
+      if string.include?("\n")
+        lines = string.split(/\n/, -1)
+        CharPos.new(@line + lines.length - 1, lines.last.length, @index + string.length)
+      else
+        CharPos.new(@line, @char + string.length, @index + string.length)
+      end
+    end
+
+    def to_s
+      "{line:#{@line} char:#{@char} index:#{@index}}"
+    end
+
+    # A re-usable empty position
+    EMPTY = CharPos.new(1,0,-1)
+  end
+end

lib/rkelly/char_range.rb
@@ -1,0 +1,29 @@
+require 'rkelly/char_pos'
+
+module RKelly
+  # Represents a syntax element location in source code - where it
+  # begins and where it ends.
+  #
+  # It's a value object - it can't be modified.
+  class CharRange
+    attr_reader :from, :to
+
+    def initialize(from, to)
+      @from = from
+      @to = to
+    end
+
+    # Creates a new range that immediately follows this one and
+    # contains the given string.
+    def next(string)
+      CharRange.new(@to.next(string.slice(0, 1)), @to.next(string))
+    end
+
+    def to_s
+      "<#{@from} .. #{@to}>"
+    end
+
+    # A re-usable empty range
+    EMPTY = CharRange.new(CharPos::EMPTY, CharPos::EMPTY)
+  end
+end

lib/rkelly/tokenizer.rb
@@ -1,4 +1,5 @@
 require 'rkelly/lexeme'
+require 'rkelly/char_range'
 require 'strscan'
 
 module RKelly
@@ -115,7 +116,7 @@ module RKelly
     def raw_tokens(string)
       scanner = StringScanner.new(string)
       tokens = []
-      position = [1, 1]
+      range = CharRange::EMPTY
       accepting_regexp = true
       while !scanner.eos?
         longest_token = nil
@@ -134,16 +135,9 @@ module RKelly
           accepting_regexp = followable_by_regex(longest_token)
         end
 
-        old_position = position
-        if longest_token.value.include?("\n")
-          lines = longest_token.value.split(/\n/, -1)
-          position = [old_position[0] + lines.length - 1, lines.last.length]
-        else
-          position = [old_position[0], old_position[1] + longest_token.value.length]
-        end
-
+        range = range.next(longest_token.value)
         scanner.pos += longest_token.value.length
-        longest_token.range = [[old_position[0], old_position[1]+1], position]
+        longest_token.range = range
         tokens << longest_token
       end
       tokens

reesericci/recma6

lib/recma/char_pos.rb
@@ -1,0 +1,33 @@
+
+module RECMA
+  # Represents a character position in source code.
+  #
+  # It's a value object - it can't be modified.
+  class CharPos
+    attr_reader :line, :char, :index
+
+    def initialize(line, char, index)
+      @line = line
+      @char = char
+      @index = index
+    end
+
+    # Creates a new character position that's a given string away from
+    # this one.
+    def next(string)
+      if string.include?("\n")
+        lines = string.split(/\n/, -1)
+        CharPos.new(@line + lines.length - 1, lines.last.length, @index + string.length)
+      else
+        CharPos.new(@line, @char + string.length, @index + string.length)
+      end
+    end
+
+    def to_s
+      "{line:#{@line} char:#{@char} index:#{@index}}"
+    end
+
+    # A re-usable empty position
+    EMPTY = CharPos.new(1,0,-1)
+  end
+end

lib/recma/char_range.rb
@@ -1,0 +1,29 @@
+require 'recma/char_pos'
+
+module RECMA
+  # Represents a syntax element location in source code - where it
+  # begins and where it ends.
+  #
+  # It's a value object - it can't be modified.
+  class CharRange
+    attr_reader :from, :to
+
+    def initialize(from, to)
+      @from = from
+      @to = to
+    end
+
+    # Creates a new range that immediately follows this one and
+    # contains the given string.
+    def next(string)
+      CharRange.new(@to.next(string.slice(0, 1)), @to.next(string))
+    end
+
+    def to_s
+      "<#{@from} .. #{@to}>"
+    end
+
+    # A re-usable empty range
+    EMPTY = CharRange.new(CharPos::EMPTY, CharPos::EMPTY)
+  end
+end

lib/recma/tokenizer.rb
@@ -1,4 +1,5 @@
 require 'recma/lexeme'
+require 'recma/char_range'
 require 'strscan'
 
 module RECMA
@@ -115,7 +116,7 @@ module RECMA
     def raw_tokens(string)
       scanner = StringScanner.new(string)
       tokens = []
-      position = [1, 1]
+      range = CharRange::EMPTY
       accepting_regexp = true
       while !scanner.eos?
         longest_token = nil
@@ -134,16 +135,9 @@ module RECMA
           accepting_regexp = followable_by_regex(longest_token)
         end
 
-        old_position = position
-        if longest_token.value.include?("\n")
-          lines = longest_token.value.split(/\n/, -1)
-          position = [old_position[0] + lines.length - 1, lines.last.length]
-        else
-          position = [old_position[0], old_position[1] + longest_token.value.length]
-        end
-
+        range = range.next(longest_token.value)
         scanner.pos += longest_token.value.length
-        longest_token.range = [[old_position[0], old_position[1]+1], position]
+        longest_token.range = range
         tokens << longest_token
       end
       tokens