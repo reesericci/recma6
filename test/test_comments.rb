tenderlove/recma

Add all comments to root node of the AST.
Instead of attempting to associate all comments with syntax nodes
(and actually failing to correctly do so) all comments found are
added to the root SoureElementsNode.comments - all other nodes
will have an empty #comments array.

test/test_comments.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/helper"
 
 class CommentsTest < NodeTestCase
-  def disabled_test_some_comments
+  def test_some_comments
     parser = RKelly::Parser.new
     ast = parser.parse(<<-eojs)
       /**
@@ -13,17 +13,14 @@ class CommentsTest < NodeTestCase
       }
     eojs
 
-    func = ast.pointcut(FunctionDeclNode).matches.first
-    assert func
-    assert_match('awesome', func.comments[0].value)
-    assert_match('side', func.comments[1].value)
-
-    return_node = ast.pointcut(ReturnNode).matches.first
-    assert return_node
-    assert_match('America', return_node.comments[0].value)
+    assert ast
+    assert_equal(3, ast.comments.length)
+    assert_match('awesome', ast.comments[0].value)
+    assert_match('side', ast.comments[1].value)
+    assert_match('America', ast.comments[2].value)
   end
 
-  def disabled_test_even_more_comments
+  def test_only_comments
     parser = RKelly::Parser.new
     ast = parser.parse(<<-eojs)
       /**
@@ -32,13 +29,17 @@ class CommentsTest < NodeTestCase
       /**
        * This is an awesome test comment.
        */
-      function aaron() { // This is a side comment
-        var x = 10;
-        return 1 + 1; // America!
-      }
     eojs
-    func = ast.pointcut(FunctionDeclNode).matches.first
-    assert func
-    assert_equal(3, func.comments.length)
+
+    assert ast
+    assert_equal(2, ast.comments.length)
+  end
+
+  def test_empty_source_results_in_zero_comments
+    parser = RKelly::Parser.new
+    ast = parser.parse("")
+
+    assert ast
+    assert_equal(0, ast.comments.length)
   end
 end

reesericci/recma6

test/test_comments.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/helper"
 
 class CommentsTest < NodeTestCase
-  def disabled_test_some_comments
+  def test_some_comments
     parser = RECMA::Parser.new
     ast = parser.parse(<<-eojs)
       /**
@@ -13,17 +13,14 @@ class CommentsTest < NodeTestCase
       }
     eojs
 
-    func = ast.pointcut(FunctionDeclNode).matches.first
-    assert func
-    assert_match('awesome', func.comments[0].value)
-    assert_match('side', func.comments[1].value)
-
-    return_node = ast.pointcut(ReturnNode).matches.first
-    assert return_node
-    assert_match('America', return_node.comments[0].value)
+    assert ast
+    assert_equal(3, ast.comments.length)
+    assert_match('awesome', ast.comments[0].value)
+    assert_match('side', ast.comments[1].value)
+    assert_match('America', ast.comments[2].value)
   end
 
-  def disabled_test_even_more_comments
+  def test_only_comments
     parser = RECMA::Parser.new
     ast = parser.parse(<<-eojs)
       /**
@@ -32,13 +29,17 @@ class CommentsTest < NodeTestCase
       /**
        * This is an awesome test comment.
        */
-      function aaron() { // This is a side comment
-        var x = 10;
-        return 1 + 1; // America!
-      }
     eojs
-    func = ast.pointcut(FunctionDeclNode).matches.first
-    assert func
-    assert_equal(3, func.comments.length)
+
+    assert ast
+    assert_equal(2, ast.comments.length)
+  end
+
+  def test_empty_source_results_in_zero_comments
+    parser = RECMA::Parser.new
+    ast = parser.parse("")
+
+    assert ast
+    assert_equal(0, ast.comments.length)
   end
 end